a21a485b8e40f19157e9b928124fcff73816a75c
Luke Lighthart <Cu-Sith@gmx.com>
2021-05-05 12:25:56 -0400

    Working on Nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
+import './App.scss';
+import { Nav } from "./components"
 
 function App() {
-  return (<></>
+  return (
+    <>
+      <Nav />
+    </>
   );
 }
 
---
+++ src/components/drawer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function NavDrawer(props) {
+    const [open, toggleOpen] = React.useState({
+        open: false
+    })
+    return (
+        
+    )
+}
